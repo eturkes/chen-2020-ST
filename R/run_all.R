4c3d64f1645248f4f1222e1c3ad57711279f12fe
Emir Turkes <eturkes@bu.edu>
2020-10-06 14:09:53 +0100

    Use different delimiter for HTML files

--- R/run_all.R
+++ R/run_all.R
@@ -25,22 +25,22 @@ dir_name <- "01-all"
 save_name <- "01_prep"
 rmarkdown::render(
   file.path(dir_name, paste0(save_name, ".Rmd")),
-  output_file = file.path("..", "..", "results", paste0(save_name, ".html"))
+  output_file = file.path("..", "..", "results", paste0(gsub("_", "-", save_name), ".html"))
 )
 save_name <- "02_analysis"
 rmarkdown::render(
   file.path(dir_name, paste0(save_name, ".Rmd")),
-  output_file = file.path("..", "..", "results", paste0(save_name, ".html"))
+  output_file = file.path("..", "..", "results", paste0(gsub("_", "-", save_name), ".html"))
 )
 
 dir_name <- "01-EC-Reln"
 save_name <- "01_prep"
 rmarkdown::render(
   file.path(dir_name, paste0(save_name, ".Rmd")),
-  output_file = file.path("..", "..", "results", paste0(save_name, ".html"))
+  output_file = file.path("..", "..", "results", paste0(gsub("_", "-", save_name), ".html"))
 )
 save_name <- "02_analysis"
 rmarkdown::render(
   file.path(dir_name, paste0(save_name, ".Rmd")),
-  output_file = file.path("..", "..", "results", paste0(save_name, ".html"))
+  output_file = file.path("..", "..", "results", paste0(gsub("_", "-", save_name), ".html"))
 )
